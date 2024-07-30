748427791307a6470940662a076c08e272794e42
EEJefferson <161738186+EEJefferson@users.noreply.github.com>
2024-07-30 17:05:08 +0300

    feat: Update

--- src/index.js
+++ src/index.js
@@ -34,10 +34,10 @@ module.exports = function toReadable (number) {
     if(str.length===2) return `${via[str[0+'0']]} ${via[str[1]]}`;
     if (str.slice(-2) == "00") return `${via[str[0]]} hundred`;
     if (str.length===3&&str.slice(-1)!=='0') {
-        `${via[str[0]]} hundred ${via[str[1]+'0']} ${via[str[2]]}`
+        return `${via[str[0]]} hundred ${via[str[1]+'0']} ${via[str[2]]}`
     } else if(str.length===3&&str.slice(-1)=='0'){
-        `${via[str[0]]} hundred ${via[str[1]+'0']}`
+        return `${via[str[0]]} hundred ${via[str[1]+'0']}`
     } else if(via[str[1]]==='0'){
-        `${via[str[0]]} hundred ${via[str[2]]}`
+        return `${via[str[0]]} hundred ${via[str[2]]}`
     }
 }
