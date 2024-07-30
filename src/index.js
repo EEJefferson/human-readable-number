7269c0b4f478ce024a6ec677d7a98f42359160e6
EEJefferson <161738186+EEJefferson@users.noreply.github.com>
2024-07-30 17:11:50 +0300

    feat: update code

--- src/index.js
+++ src/index.js
@@ -1,43 +1,54 @@
-module.exports = function toReadable (number) {
+module.exports = function toReadable(number) {
   const via = {
-        0: "zero",
-        1: "one",
-        2: "two",
-        3: "three",
-        4: "four",
-        5: "five",
-        6: "six",
-        7: "seven",
-        8: "eight",
-        9: "nine",
-        10: "ten",
-        11: "eleven",
-        12: "twelve",
-        13: "thirteen",
-        14: "fourteen",
-        15: "fifteen",
-        16: "sixteen",
-        17: "seventeen",
-        18: "eighteen",
-        19: "nineteen",
-        20: "twenty",
-        30: "thirty",
-        40: "forty",
-        50: "fifty",
-        60: "sixty",
-        70: "seventy",
-        80: "eighty",
-        90: "ninety",
-    };
-    let str = number.toString();
-    if(via[str]) return via[str];
-    if(str.length===2) return `${via[str[0+'0']]} ${via[str[1]]}`;
-    if (str.slice(-2) == "00") return `${via[str[0]]} hundred`;
-    if (str.length===3&&str.slice(-1)!=='0') {
-        return `${via[str[0]]} hundred ${via[str[1]+'0']} ${via[str[2]]}`
-    } else if(str.length===3&&str.slice(-1)=='0'){
-        return `${via[str[0]]} hundred ${via[str[1]+'0']}`
-    } else if(str[1]==='0'){
-        return `${via[str[0]]} hundred ${via[str[2]]}`
+    0: "zero",
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine",
+    10: "ten",
+    11: "eleven",
+    12: "twelve",
+    13: "thirteen",
+    14: "fourteen",
+    15: "fifteen",
+    16: "sixteen",
+    17: "seventeen",
+    18: "eighteen",
+    19: "nineteen",
+    20: "twenty",
+    30: "thirty",
+    40: "forty",
+    50: "fifty",
+    60: "sixty",
+    70: "seventy",
+    80: "eighty",
+    90: "ninety",
+  };
+
+  let str = number.toString();
+
+  if (via[str]) return via[str];
+
+  if (str.length === 2) {
+    const tens = str[0] + '0';
+    const units = str[1];
+    return `${via[tens]} ${via[units]}`;
+  }
+
+  if (str.length === 3) {
+    const hundreds = via[str[0]] + ' hundred';
+    const tensUnits = str.slice(1);
+    if (tensUnits === '00') {
+      return hundreds;
+    } else if (tensUnits[1] === '0') {
+      return `${hundreds} ${via[tensUnits[0] + '0']}`;
+    } else {
+      return `${hundreds} ${via[tensUnits[0] + '0']} ${via[tensUnits[1]]}`;
     }
-}
+  }
+};
